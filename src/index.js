2a48cc6e5a12c409ad663b2c8e8c84c62d1b7d35
chimezie-git <66944677+chimezie-git@users.noreply.github.com>
2020-07-15 12:05:58 -0400

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import './main.css';
+import './bootstrap.min.css';
+import './css/styles/form.css';
+import styles from './index.module.css';
+
+const h1 = document.querySelector('h1');
+h1.classList.add(styles.h1);
+
+async function print() {
+  // Here we are using dynamic import
+  const { greet } = await import('./greet');
+  const response = await greet('John Doe');
+  console.log(response);
+}
+
+print();
